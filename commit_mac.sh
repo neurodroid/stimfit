5348487beadc6e75ca1aca466bef5afac9b6f2b1
Christoph Schmidt-Hieber <christsc@gmx.de>
2009-05-04 08:38:34 +0100

    Updated commit script.

--- commit_mac.sh
+++ commit_mac.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-REMOTE=https://christsc\@gmx.de\@stimfit.googlecode.com/hg/stimfit/
+REMOTE=https://christsc\@gmx.de:Xe9Vr2Jf2Xd9\@stimfit.googlecode.com/hg/stimfit/
 echo hg addremove
 hg addremove
 echo hg ci
