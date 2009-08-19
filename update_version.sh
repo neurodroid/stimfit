7349865cb69dc1dddd76387c179629ae73342412
Christoph Schmidt-Hieber <christsc@gmx.de>
2009-08-19 22:19:54 +0100

    Updated web site.

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" \) -exec sed -i 's/'$1'/'$2'/' {} \;
-find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" \) -exec sed -i 's/'$1'/'$2'/' {} \;
+echo find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" \) -exec sed -i 's/'$1'/'$2'/' {} \;
+find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" \) -exec sed -i 's/'$1'/'$2'/' {} \;
