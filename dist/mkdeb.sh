494f173793382c43e160c73cadc4c2273e6f0ceb
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-01-24 19:12:03 +0000

    Bumped version to 0.10.6

--- dist/mkdeb.sh
+++ dist/mkdeb.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 make dist
-cp stimfit-0.10.5.tar.gz ../deb/
-cp stimfit-0.10.5.tar.gz ../deb/stimfit_0.10.5.orig.tar.gz
+mkdir -p ../deb/
+cp -v stimfit-0.10.6.tar.gz ../deb/
+cp -v stimfit-0.10.6.tar.gz ../deb/stimfit_0.10.5.orig.tar.gz
 cd ../deb/
-tar -xzf stimfit_0.10.5.orig.tar.gz
-cd stimfit-0.10.5
+tar -xzf stimfit_0.10.6.orig.tar.gz
+cd stimfit-0.10.6
+mkdir -p debian
+cp -rv ../../../dist/debian/* ./debian
 debuild -S -sa
 sudo pbuilder build ../*.dsc
