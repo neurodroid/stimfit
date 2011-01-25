8157fc1ea926bdbd467d108c74055a188a131cb4
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-01-25 09:29:46 +0000

    Edited mkdeb script

--- dist/mkdeb.sh
+++ dist/mkdeb.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+VERSION=0.10.6
+
 make dist
 mkdir -p ../deb/
-cp -v stimfit-0.10.6.tar.gz ../deb/
-cp -v stimfit-0.10.6.tar.gz ../deb/stimfit_0.10.5.orig.tar.gz
+cp -v stimfit-${VERSION}.tar.gz ../deb/
+cp -v stimfit-${VERSION}.tar.gz ../deb/stimfit_${VERSION}.orig.tar.gz
 cd ../deb/
-tar -xzf stimfit_0.10.6.orig.tar.gz
-cd stimfit-0.10.6
+tar -xzf stimfit_${VERSION}.orig.tar.gz
+cd stimfit-${VERSION}
 mkdir -p debian
 cp -rv ../../../dist/debian/* ./debian
 debuild -S -sa
