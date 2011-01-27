a0f4a19c4a8a4d071bdac4fa2e204f5a99d9959d
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-01-27 19:54:47 +0000

    Debian package update

--- debian/mkdeb.sh
+++ debian/mkdeb.sh
@@ -4,12 +4,13 @@ VERSION=0.10.6
 
 make dist
 mkdir -p ../deb/
+rm -rf ../deb/*
 cp -v stimfit-${VERSION}.tar.gz ../deb/
 cp -v stimfit-${VERSION}.tar.gz ../deb/stimfit_${VERSION}.orig.tar.gz
 cd ../deb/
 tar -xzf stimfit_${VERSION}.orig.tar.gz
 cd stimfit-${VERSION}
 mkdir -p debian
-cp -rv ../../../dist/debian/* ./debian
+cp -rv ../../../debian/* ./debian
 debuild -S -sa
 sudo pbuilder build ../*.dsc
