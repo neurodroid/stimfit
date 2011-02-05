ea584347a0d3d099fd09c7c7f1f3ff37b0605055
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-02-05 10:57:49 +0000

    Addressed remaining lintian complaints

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o name "mkdeb.sh" \) -exec sed -i 's/'$1'/'$2'/' {} \;
-find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o -name "mkdeb.sh" \) -exec sed -i 's/'$1'/'$2'/' {} \;
+echo find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o name "mkdeb.sh" -o name "rules" \) -exec sed -i 's/'$1'/'$2'/' {} \;
+find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.in" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o -name "mkdeb.sh" -o name "rules" \) -exec sed -i 's/'$1'/'$2'/' {} \;
