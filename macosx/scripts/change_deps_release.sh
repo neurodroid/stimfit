6ef4dbce28e13b404f357f72434ce6d0bc868820
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-02-27 18:47:32 +0000

    Updated OS X build

--- macosx/scripts/change_deps_release.sh
+++ macosx/scripts/change_deps_release.sh
@@ -5,7 +5,7 @@ WX_CONFIG=/Users/cs/wxbin/bin/wx-config
 # WXPY_VER=wx-2.9.1-osx_cocoa
 # WXPY_INSTALL_DIR=/Users/cs/wxPython-2.9/dummy-install/lib/python2.5/site-packages
 
-sudo chown -R cs:staff stimfit.app
+chown -R cs:staff stimfit.app
 
 make stimfit.app
 mkdir -p ./stimfit.app/Contents/Frameworks/stimfit
@@ -25,36 +25,29 @@ mkdir -p ./stimfit.app/Contents/Frameworks/stimfit
 # fi
 
 ##
-rsync -rtuvl `${WX_CONFIG} --exec-prefix`/lib/libwx*.dylib ./stimfit.app/Contents/libs/
-sudo mkdir -p /usr/local/lib/stimfit
-sudo cp -v ./src/stfswig/.libs/libstf.dylib /usr/local/lib/stimfit/libstf.dylib
+# rsync -rtuvl `${WX_CONFIG} --exec-prefix`/lib/libwx*.dylib ./stimfit.app/Contents/libs/
+mkdir -p ./stimfit.app/Contents/lib/stimfit
+cp -v ./src/stfswig/.libs/libstf.dylib ./stimfit.app/Contents/lib/stimfit/libstf.dylib
+cp -v ./src/app/.libs/libstimfit.dylib ./stimfit.app/Contents/lib/stimfit/libstimfit.dylib
 rm -f ./stimfit.app/Contents/Frameworks/stimfit/_stf.so
-cp -v ./src/stfswig/.libs/libstf.dylib ./stimfit.app/Contents/Frameworks/stimfit/_stf.so
+CURDIR=`pwd`
+cd stimfit.app/Contents/Frameworks/stimfit
+ln -sf ../../libs/libstf.dylib _stf.so
+cd ${CURDIR}
 rm -f ./stimfit.app/Contents/libs/libstf.dylib
+rm -f ./stimfit.app/Contents/libs/libstimfit.dylib
+cp -v ./src/app/.libs/libstimfit.dylib ./stimfit.app/Contents/libs/libstimfit.dylib
 dylibbundler -of -b -x ./stimfit.app/Contents/MacOS/stimfit -d ./stimfit.app/Contents/libs/
-##
 
-# sudo mv ./stimfit.app/Contents/libs/libstf.dylib /usr/local/lib/stimfit/_stf.so
-# ln -sf /usr/local/lib/stimfit/_stf.so ./stimfit.app/Contents/libs/libstf.dylib
-
-##
 if test -n "$1"; then
   if [ $1 = '1' ]; then
     find ./stimfit.app  -name "*.dylib" -exec dylibbundler -of -b -x '{}' -d ./stimfit.app/Contents/libs/ \;
     find ./stimfit.app  -name "*.so" -exec dylibbundler -of -b -x '{}' -d ./stimfit.app/Contents/libs/ \;
   fi
 fi
-sudo rm /usr/local/lib/stimfit
-
-##
-
-# ln -sf ./stimfit.app/Contents/libs/libstf.dylib ./stimfit.app/Contents/Frameworks/stimfit/_stf.so 
+rm -rf ./stimfit.app/Contents/lib/stimfit
 
 cp -v ../../src/stfswig/*.py ./stimfit.app/Contents/Frameworks/stimfit/
-
-
-# # sudo rm /usr/local/lib/libwx*.dylib
-# # sudo chown -R root:admin stimfit.app 
 
 # # rm ./stimfit.app/Contents/libs/libwx*
 # # rsync -rtuvl `${WX_CONFIG} --exec-prefix`/lib/libwx_baseu_net-* ./stimfit.app/Contents/libs/
