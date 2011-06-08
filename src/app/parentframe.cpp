be45a1eba892a48aa3e02671a0b388dc164b8ba5
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-06-08 22:52:20 +0100

    Compilation fix

--- src/app/parentframe.cpp
+++ src/app/parentframe.cpp
@@ -772,7 +772,7 @@ void wxStfParentFrame::OnConvert(wxCommandEvent& WXUNUSED(event) ) {
                      stf::exportATFFile( destFilename, sourceFile );
                      break;
                  case stf::igor:
-                     stf::exportIGORFile( std::string(destFilename), sourceFile );
+                     stf::exportIGORFile( destFilename, sourceFile );
                      break;
                  default:
                      wxString errorMsg(wxT("Unknown export file type\n"));
