50084bfe8e335468177d65f65994f9c202436da7
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-07-24 18:35:06 +0100

    Bumped version to 0.10.16. Prepared debian release.

---
+++ debian/upload-deb.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+VERSION=0.10.16
+
+cp -v ../deb/stimfit_${VERSION}-1.debian.tar.gz /media/webftp/StimfitJ/deb/
+cp -v ../deb/stimfit_${VERSION}-1.dsc /media/webftp/StimfitJ/deb/
+cp -v ../deb/stimfit_${VERSION}.orig.tar.gz /media/webftp/StimfitJ/deb/
