f6ad9c6e4a60d58f137df016db235c985cda36a3
Adrian Bunk <bunk@stusta.de>
2012-04-08 02:11:50 +0300

    Replace shave with automake's silent rules. Fixes issue #20
    Signed-off-by: Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>

--- macosx/scripts/conf_macports_release.sh
+++ macosx/scripts/conf_macports_release.sh
@@ -7,7 +7,6 @@ MYCXX=`${prefix}/bin/wx-config --cxx`
 MYLD=`${prefix}/bin/wx-config --ld`
 
 config_args="--with-wx-config=${prefix}/bin/wx-config \
-                    --disable-dependency-tracking \
-                    --disable-shave"
+                    --disable-dependency-tracking"
                     
 ../../configure ${config_args} CC="${MYCC} -I${prefix}/include" CXX="${MYCXX} -I${prefix}/include" LD="${MYLD}" LDFLAGS="-headerpad_max_install_names -L${prefix}/lib"
