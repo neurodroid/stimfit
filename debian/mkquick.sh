7e7892aac5f8ad9671824fda1198ec1644a23576
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2012-12-16 17:25:52 +0000

    Bump version to 0.11.5

--- debian/mkquick.sh
+++ debian/mkquick.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=0.11.4
+VERSION=0.11.5
 
 make dist
 mkdir -p ../deb/
--- macosx/scripts/mkimage.sh
+++ macosx/scripts/mkimage.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-hdiutil create stimfit-0.11.4.dmg -srcfolder ./stimfit.app -ov -format UDBZ
+hdiutil create stimfit-0.11.5.dmg -srcfolder ./stimfit.app -ov -format UDBZ
