aa61cc3a16f77c53502ea9ca247e3081f9855726
Jose Guzman <jguzman@laptop>
2013-05-12 18:50:10 +0200

    Add lmfit test case for monoexponential with delay, start fixed to baseline

--- src/test/fit.cpp
+++ src/test/fit.cpp
@@ -3,6 +3,7 @@
 #include "../stimfit/math/funclib.h"
 #include <gtest/gtest.h>
 #include <cmath>
+#include <fstream>
 
 
 const static int nmax = 32768;
@@ -10,11 +11,39 @@ const static double tol = 0.1;
 
 /* global variables to define our data */
 const static int tmax = 100;   /* length of data in ms */
-const static float dt = 1/100.; /* sampling interval of data in ms */
+const static float dt = 1/100.0; /* sampling interval of data in ms */
 
 /* list of available fitting functions */
 const static std::vector< stf::storedFunc > funcLib = stf::GetFuncLib();
 
+//=========================================================================
+// 03 - Monoexponential function with delay, start fixed to baseline 
+// param is an array of parameters, where
+// param[0] is the baseline, 
+// param[1] is the delay,
+// param[2] is the time constant, 
+// param[3] is the amplitude
+//=========================================================================
+
+Vector_double fexpde(const Vector_double &param){
+    Vector_double mydata (int (tmax/dt));
+
+    for (int n=0; n<mydata.size(); ++n){
+        mydata[n] = stf::fexpde(n*dt, param);
+    }
+    
+    return mydata;
+}
+
+//=========================================================================
+// 05 - Biexponential function with offset to baseline
+// param is an array of parameters, where
+// param[0] is the baseline, 
+// param[1] is the delay,
+// param[2] is the slow time constant (e.g tau_h, tau_2)
+// param[3] is the factor,
+// param[4] is the fast time constant (e.g tau_m, tau_1)
+//=========================================================================
 Vector_double fexpbde(const Vector_double &param){
     Vector_double mydata (int(tmax/dt));
     
@@ -25,8 +54,22 @@ Vector_double fexpbde(const Vector_double &param){
     return mydata;
 }
 
-#if 0
-void debug_stdout(double chisqr, const std::string& info, int warning, const Vector_double &pars){
+//#if 0
+void savetxt(const Vector_double &mydata){
+
+    std::ofstream output_file;
+    output_file.open("array.out");
+
+    for (int n=0; n<mydata.size(); ++n){
+        output_file << mydata[n] << std::endl;
+    }
+
+    output_file.close();
+}
+
+void debug_stdout(double chisqr, const std::string& info, int warning, \
+    const Vector_double &pars){
+
     int fd = open("debug.log", O_WRONLY|O_CREAT|O_TRUNC, 0660);
     assert(fd >= 0);
     int ret = dup2(fd, 1);
@@ -39,7 +82,7 @@ void debug_stdout(double chisqr, const std::string& info, int warning, const Vec
     }
     close(fd);
 }
-#endif
+//#endif
 
 void par_test(double value, double expected, double tolerance) {
     EXPECT_NEAR(value, expected, abs(expected*tolerance));
@@ -151,6 +194,55 @@ TEST(fitlib_test, monoexponential_offset2baseline){
     par_test(pars[1], tau, tol);  /* Time constant */
     par_test(pars[2], base, tol);  /* Baseline */
 }
+//=========================================================================
+// Tests fitting to a monoexponential function with delay, start to base 
+// Stimfit function with ID = 3
+//=========================================================================
+TEST(fitlib_test, id_03_monoexponential_with_delay){
+
+    /* choose function parameters */
+    Vector_double mypars(4);
+    mypars[0] = 10.0; /* baseline */
+    mypars[1] = 15.0; /* delay */
+    mypars[2] = 17.0; /* time constant */
+    mypars[3] = 90.0; /* amplitude */
+
+    /* create a 100 ms trace with mypars */
+    Vector_double data;
+    data = fexpde(mypars);
+
+#if 0
+    savetxt(data);
+#endif
+
+    /* options for the implementation of the LM algorithm */
+    Vector_double opts(6);
+    opts[0] = 5*1E-3; opts[1] = 1E-17; opts[2] = 1E-17;
+    opts[3] = 1E-32; opts[4] = 64; opts[5] = 16;
+
+    /* Initial parameter guesses */
+    Vector_double pars(4);
+    pars[0] = mypars[0]; 
+    pars[1] = 5.0;
+    pars[2] = 50.0;
+    pars[3] = 21.0;
+
+    std::string info;
+    int warning;
+    double chisqr = lmFit(data, dt, funcLib[2], opts, true, pars, \
+        info, warning );
+
+    EXPECT_EQ(warning, 0);
+    par_test(pars[0], mypars[0], tol); /* baseline */
+    par_test(pars[1], mypars[1], tol); /* delay */
+    par_test(pars[2], mypars[2], tol); /* time constant */
+    par_test(pars[3], mypars[3], tol); /* amplitude */
+
+#if 0
+    debug_stdout(chisqr, info, warning, pars);
+#endif
+}
+
 
 //=========================================================================
 // Tests fitting to a biexponential function, offset fixed to baseline 
@@ -158,7 +250,7 @@ TEST(fitlib_test, monoexponential_offset2baseline){
 //=========================================================================
 TEST(fitlib_test, id_05_biexponentialoffset2baseline){
     
-    /* choose function parameteres */
+    /* choose function parameters */
     Vector_double mypars(5);
     mypars[0] = 0.0;     /* baseline */
     mypars[1] = 25.0;    /* Delay    */
@@ -182,7 +274,6 @@ TEST(fitlib_test, id_05_biexponentialoffset2baseline){
     pars[2] = 19.925;         /* tau_1    */
     pars[3] = 20.0;           /* Factor   */
     pars[4] = 24.9875;        /* tau_2    */
-
 
     std::string info;
     int warning;
