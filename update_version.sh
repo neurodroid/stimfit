08af2cec25f9298f364c949ff35f76b125765902
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-05-23 14:53:23 +0100

    Use gsed if available to update version codes

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-echo find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.ac" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o -name "mkdeb.sh" -o -name "update-deb.sh" -o -name "mkquick.sh" -o -name "mkimage.sh" -o -name "stimfit.1" -o -name "Doxyfile" -o -name insert_checksums.sh \) -exec sed -i 's/'$1'/'$2'/' {} \;
-find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.ac" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o -name "mkdeb.sh" -o -name "update-deb.sh" -o -name "mkquick.sh" -o -name "mkimage.sh" -o -name "stimfit.1" -o -name "Doxyfile" -o -name insert_checksums.sh \) -exec sed -i 's/'$1'/'$2'/' {} \;
+#! /bin/bash
+GSED=`which gsed`
+if [ "${GSED}" = "" ]
+then
+    GSED=`which sed`
+fi
+
+echo find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.ac" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o -name "mkdeb.sh" -o -name "update-deb.sh" -o -name "mkquick.sh" -o -name "mkimage.sh" -o -name "stimfit.1" -o -name "Doxyfile" -o -name insert_checksums.sh \) -exec $GSED -i 's/'$1'/'$2'/' {} \;
+find ./ -type f  \( -name "stfconf.h" -o -name "index.xml" -o -name "configure.ac" -o -name "conf.py" -o -name "installer.nsi" -o -name "Home.html" -o -name "stimfit.plist.in" -o -name "mkdeb.sh" -o -name "update-deb.sh" -o -name "mkquick.sh" -o -name "mkimage.sh" -o -name "stimfit.1" -o -name "Doxyfile" -o -name insert_checksums.sh \) -exec $GSED -i 's/'$1'/'$2'/' {} \;
