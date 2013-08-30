6cc3ac72a3abed737d0a95b254f7ec587407a435
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-08-30 18:31:14 +0100

    Update MacPorts configure script to use new wx-config location

--- macosx/scripts/conf_macports_release.sh
+++ macosx/scripts/conf_macports_release.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 prefix="/opt/local"
+WX_CONF="${prefix}/Library/Frameworks/wxWidgets.framework/Versions/wxPython/2.9/bin/wx-config"
 
-MYCC=`${prefix}/bin/wx-config --cc`
-MYCXX=`${prefix}/bin/wx-config --cxx`
-MYLD=`${prefix}/bin/wx-config --ld`
+MYCC=`${WX_CONF} --cc`
+MYCXX=`${WX_CONF} --cxx`
+MYLD=`${WX_CONF} --ld`
 
-config_args="--with-wx-config=${prefix}/bin/wx-config \
-             --disable-dependency-tracking \
-             --enable-debug"
+config_args="--with-wx-config=${WX_CONF} \
+             --disable-dependency-tracking"
                     
-../../configure ${config_args} CC="${MYCC} -I${prefix}/include" CXX="${MYCXX} -I${prefix}/include" LD="${MYLD}" LDFLAGS="-headerpad_max_install_names -L${prefix}/lib" PYTHON="${prefix}/bin/python3.3"
+../../configure ${config_args} CC="${MYCC} -I${prefix}/include" CXX="${MYCXX} -I${prefix}/include" LD="${MYLD}" LDFLAGS="-headerpad_max_install_names -L${prefix}/lib" PYTHON="${prefix}/bin/python2.7"
