0f74ce97d8a075ef0458205474776a127356ecd4
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-09-01 19:37:35 +0100

    Upgrade Portfiles to 0.12.9 with custom LAPACK option

--- macosx/scripts/conf_macports_release.sh
+++ macosx/scripts/conf_macports_release.sh
@@ -9,5 +9,6 @@ MYLD=`${WX_CONF} --ld`
 
 config_args="--with-wx-config=${WX_CONF} \
              --disable-dependency-tracking"
-                    
+# --with-lapack-lib=/opt/local/lib/libatlas.a
+
 ../../configure ${config_args} CC="${MYCC} -I${prefix}/include" CXX="${MYCXX} -I${prefix}/include" LD="${MYLD}" LDFLAGS="-headerpad_max_install_names -L${prefix}/lib" PYTHON="${prefix}/bin/python2.7"
