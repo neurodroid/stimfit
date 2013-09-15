358363801716a074b6e3efeb389c4ef336b4242b
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-09-15 10:41:53 +0100

    Add options to macports configure script

--- macosx/scripts/conf_macports_release.sh
+++ macosx/scripts/conf_macports_release.sh
@@ -8,7 +8,9 @@ MYCXX=`${WX_CONF} --cxx`
 MYLD=`${WX_CONF} --ld`
 
 config_args="--with-wx-config=${WX_CONF} \
-             --disable-dependency-tracking"
-# --with-lapack-lib=/opt/local/lib/libatlas.a
+             --disable-dependency-tracking \
+             --with-biosig"
+#              --with-lapack-lib=/opt/local/lib/libatlas.a \
+#              --enable-debug \
 
 ../../configure ${config_args} CC="${MYCC} -I${prefix}/include" CXX="${MYCXX} -I${prefix}/include" LD="${MYLD}" LDFLAGS="-headerpad_max_install_names -L${prefix}/lib" PYTHON="${prefix}/bin/python2.7"
