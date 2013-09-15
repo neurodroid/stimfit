277206f0da18666218a94ade6170ae7a4f2e3bf7
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-09-15 10:41:34 +0100

    Disable plotting benchmark code

--- src/stimfit/gui/graph.cpp
+++ src/stimfit/gui/graph.cpp
@@ -42,6 +42,10 @@
 #include "./../math/measure.h"
 
 #ifdef _STFDEBUG
+#include <iostream>
+#endif
+
+#if 0 //def _STFDEBUG
 #include <wx/utils.h>
 #include <iostream>
 #include <fstream>
@@ -691,7 +695,7 @@ void wxStfGraph::DoPlot( wxDC* pDC, const Vector_double& trace, int start, int e
     int y_min = y_last;
     int x_next = 0;
     int y_next = 0;
-#ifdef _STFDEBUG
+#if 0 //def _STFDEBUG
     struct timespec time0, time1;
     current_utc_time(&time0);
 #else
@@ -705,7 +709,7 @@ void wxStfGraph::DoPlot( wxDC* pDC, const Vector_double& trace, int start, int e
         x_last = x_next;
         y_last = y_next;
     }
-#ifdef _STFDEBUG
+#if 0 //def _STFDEBUG
     current_utc_time(&time1);
     double accum = tdiff(time1, time0)*1e3;
     std::string fn_platform = "plt_bench_" + stf::wx2std(wxGetOsDescription()) + ".txt";
@@ -746,7 +750,7 @@ void wxStfGraph::DoPlot( wxDC* pDC, const Vector_double& trace, int start, int e
             y_last = y_next;
         }
     }
-#ifdef _STFDEBUG
+#if 0 //def _STFDEBUG
     current_utc_time(&time1);
     accum = tdiff(time1, time0)*1e3;
     plt_bench << accum << std::endl;
