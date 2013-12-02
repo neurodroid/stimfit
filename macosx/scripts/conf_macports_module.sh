ae4a15e679599885bc984a40c8b431be90743dd2
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2013-12-02 10:47:16 +0000

    Test stfio module with biosig on OS X

--- macosx/scripts/conf_macports_module.sh
+++ macosx/scripts/conf_macports_module.sh
@@ -7,7 +7,8 @@ MYCC=/usr/bin/clang
 MYCXX=/usr/bin/clang++
 MYLD=ld
 
-config_args=" --disable-dependency-tracking \
-                    --enable-module"
+config_args="--disable-dependency-tracking \
+             --enable-module \
+             --with-biosig"
 
 ../../configure ${config_args} CC="${MYCC} -I${prefix}/include" CXX="${MYCXX} -I${prefix}/include" LD="${MYLD}" LDFLAGS="-headerpad_max_install_names -L${prefix}/lib" PYTHON="${prefix}/bin/python${PYVER}"
