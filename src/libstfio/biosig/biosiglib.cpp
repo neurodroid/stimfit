9e0c244bbd04e5da7f29a287448e32d6fe4a62eb
Alois Schloegl <alois.schloegl@ist.ac.at>
2013-12-19 00:43:52 +0100

    importBIOSIG: shows FPulse scriptlines in file information dialog - requires libbiosig v1.6.0 or later

--- src/libstfio/biosig/biosiglib.cpp
+++ src/libstfio/biosig/biosiglib.cpp
@@ -270,7 +270,8 @@ stfio::filetype stfio::importBiosigFile(const std::string &fName, Recording &Ret
     Desc += std::string ("\nUser specified Annotations:\n")+annotationTableDesc;
 
     ReturnData.SetFileDescription(Desc);
-    //ReturnData.SetGlobalSectionDescription(Desc);
+    // hdr->AS.bci2000 is an alias to hdr->AS.fpulse, which available only in libbiosig v1.6.0 and later
+    ReturnData.SetGlobalSectionDescription(hdr->AS.bci2000 + '\0');
 
     ReturnData.SetXScale(1000.0/biosig_get_samplerate(hdr));
     ReturnData.SetXUnits("ms");
@@ -497,7 +498,8 @@ stfio::filetype stfio::importBiosigFile(const std::string &fName, Recording &Ret
         }
     }
     ReturnData.SetFileDescription(Desc);
-    //ReturnData.SetGlobalSectionDescription(Desc);
+    // hdr->AS.bci2000 is an alias to hdr->AS.fpulse, which available only in libbiosig v1.6.0 or later
+    ReturnData.SetGlobalSectionDescription(hdr->AS.bci2000 + '\0');
 
     ReturnData.SetXScale(1000.0/hdr->SampleRate);
     ReturnData.SetXUnits("ms");
