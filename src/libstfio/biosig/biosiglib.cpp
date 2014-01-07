1c3acded0610e2b6a71e86997ef8a1567391ae83
Alois Schloegl <alois.schloegl@ist.ac.at>
2014-01-07 01:26:56 +0100

    use getter-function for reading FPulse ScriptBlock

--- src/libstfio/biosig/biosiglib.cpp
+++ src/libstfio/biosig/biosiglib.cpp
@@ -271,7 +271,9 @@ stfio::filetype stfio::importBiosigFile(const std::string &fName, Recording &Ret
 
     ReturnData.SetFileDescription(Desc);
     // hdr->AS.bci2000 is an alias to hdr->AS.fpulse, which available only in libbiosig v1.6.0 and later
-    if (hdr->AS.bci2000) ReturnData.SetGlobalSectionDescription(hdr->AS.bci2000 + '\0');
+#if (BIOSIG_VERSION > 10509)
+    if (hdr->AS.bci2000) ReturnData.SetGlobalSectionDescription(biosig_get_application_specific_information(hdr));
+#endif
 
     ReturnData.SetXScale(1000.0/biosig_get_samplerate(hdr));
     ReturnData.SetXUnits("ms");
@@ -499,7 +501,8 @@ stfio::filetype stfio::importBiosigFile(const std::string &fName, Recording &Ret
     }
     ReturnData.SetFileDescription(Desc);
     // hdr->AS.bci2000 is an alias to hdr->AS.fpulse, which available only in libbiosig v1.6.0 or later
-    if (hdr->AS.bci2000) ReturnData.SetGlobalSectionDescription(hdr->AS.bci2000 + '\0');
+
+    if (hdr->AS.bci2000) ReturnData.SetGlobalSectionDescription(hdr->AS.bci2000);
 
     ReturnData.SetXScale(1000.0/hdr->SampleRate);
     ReturnData.SetXUnits("ms");
