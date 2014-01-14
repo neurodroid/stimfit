ccb359b2899dae05daf23ac7477258e5f0b7a661
Christoph Schmidt-Hieber <christoph.schmidthieber@gmail.com>
2014-01-14 15:09:03 +0000

    Clean up whitespace

--- src/stimfit/gui/graph.cpp
+++ src/stimfit/gui/graph.cpp
@@ -45,6 +45,10 @@
 #include <iostream>
 #endif
 
+#ifdef _MSC_VER
+#include <boost/math/special_functions/fpclassify.hpp> // isnan
+using boost::math::isnan;
+#endif
 // #define BENCHMARK // uncomment to run benchmark
 
 #ifdef BENCHMARK //def _STFDEBUG
