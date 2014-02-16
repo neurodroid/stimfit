99e9c8575a6ae399ac9d66b3cc3adb69e7c611e3
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2014-02-16 13:16:41 +0000

    Upload src dist when updating Portfiles

--- dist/macosx/macports/insert_checksums.sh
+++ dist/macosx/macports/insert_checksums.sh
@@ -8,6 +8,7 @@ if [ "$1" != "" ]; then
     cd build/release
     ./conf_macports_release.sh
     make dist
+    scp stimfit-${STFVERSION}.tar.gz p8210991@schmidt-hieber.de:/kunden/homepages/32/d34288459/htdocs/StimfitJ/
     cd ${MPDIR}
 fi
 
