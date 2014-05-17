671c34117caafdcfa30a400563db60b32ebd3ef4
Jose Guzman <sjm.guzman@gmail.com>
2014-05-17 11:15:30 +0200

    Addapt Doxygen documentation to src/libstfnum

--- src/libstfnum/stfnum.h
+++ src/libstfnum/stfnum.h
@@ -17,7 +17,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-/*! \file stfmath.h
+/*! \file stfnum.h
  *  \author Christoph Schmidt-Hieber
  *  \date 2008-01-16
  *  \brief Math functions.
