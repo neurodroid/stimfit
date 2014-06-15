3f3dfc4119b9c1474c8f8d26abca22202800f19a
Christoph Schmidt-Hieber <christoph.schmidthieber@gmail.com>
2014-06-15 12:40:26 +0100

    Bump version to 0.13.18

--- dist/macosx/macports/insert_checksums.sh
+++ dist/macosx/macports/insert_checksums.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-STFVERSION="0.13.17"
+STFVERSION="0.13.18"
 MPDIR=`pwd`
 
 if [ "$1" != "" ]; then
--- dist/macosx/scripts/mkimage.sh
+++ dist/macosx/scripts/mkimage.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-hdiutil create stimfit-0.13.17.dmg -srcfolder ./stimfit.app -ov -format UDBZ
+hdiutil create stimfit-0.13.18.dmg -srcfolder ./stimfit.app -ov -format UDBZ
