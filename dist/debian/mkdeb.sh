9cab6d7c058309c920b7750ece6482b16eb76722
Christoph Schmidt-Hieber <christoph.schmidthieber@gmail.com>
2015-03-02 12:27:51 +0000

    Bump version to 0.14.3

--- dist/debian/mkdeb.sh
+++ dist/debian/mkdeb.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=0.14.2
+VERSION=0.14.3
 
 make dist
 mkdir -p ../deb/
--- dist/macosx/macports/insert_checksums.sh
+++ dist/macosx/macports/insert_checksums.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-STFVERSION="0.14.2"
+STFVERSION="0.14.3"
 MPDIR=`pwd`
 
 if [ "$1" != "" ]; then
