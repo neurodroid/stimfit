2397f46f06c8ca18121ab644656602671af9dd34
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2018-06-28 09:10:47 +0200

    Update documentation

--- doc/sphinx/upload.sh
+++ doc/sphinx/upload.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-rsync -av ./.build/html/ $1@schmidt-hieber.de:/kunden/homepages/32/d34288459/htdocs/StimfitJ/doc/sphinx
+rsync -av ./.build/html/ $1@home34288459.1and1-data.host:/kunden/homepages/32/d34288459/htdocs/StimfitJ/doc/sphinx
