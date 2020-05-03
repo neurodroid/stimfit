2fc844da3e4819cd66d2a728030f5b28f111e070
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-03 18:06:17 +0000

    local changes.

---
+++ .ci/README.md
@@ -0,0 +1,15 @@
+---
+author: Dilawar Singh <dilawars@ncbs.res.in>
+date: May 2020
+---
+
+This directory contains scripts which may be used to build __and install__ this
+software on different platforms. Be careful, these were written for testing
+the build system and often ran in isolated docker container. You can refer to
+them for required softwares and steps.
+
+For example, on a Ubuntu system, run the following command in a Ubuntu container.
+
+`
+$ ./build_stimfit_ubuntu.sh
+`
--- .ci/build_stimfit_suse.sh
+++ .ci/build_stimfit_suse.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
+set -x
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# These are required for building python module.
 sudo zypper install -y cmake gcc-c++ make \
-       python3-devel \
-       python3-numpy-devel \
-       wxWidgets-devel \
-       swig hdf5-devel blas-devel lapack-devel \
-       fftw3-devel python3-wheel \
-       python3-setuptools python3-pip\
-       python3-sip-devel python3-wxPython
+         zlib-devel \
+         python3-devel \
+         python3-numpy-devel \
+         swig hdf5-devel blas-devel lapack-devel \
+         fftw3-devel python3-wheel \
+         python3-setuptools python3-pip\
+         python3-sip-devel
+
+# These are optional and are required for the GUI.
+sudo zypper install -y wxWidgets-devel python3-wxPython  || echo "GUI won't build"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (
     cd $SCRIPT_DIR
-    mkdir _opensuse 
-    cd _opensuse
-    # cmake ../..
-    /usr/bin/cmake ../../  -DCMAKE_INSTALL_PREFIX:PATH=/usr \
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    mkdir -p _opensuse && cd _opensuse
+    cmake ../..  -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo
     make -j$(nproc)
+    sudo make install
 )
+
+python3 -c "import stfio; print(dir(stfio))"
