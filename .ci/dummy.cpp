0e480455066221a3a2b39c31d496f414f0203cf8
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-05 20:46:44 +0530

    Create a dummy binary stimfit when gui can not be built.  fixed typo.

---
+++ .ci/dummy.cpp
@@ -0,0 +1,16 @@
+/***
+ *    Description:  A dummy target for stimfit when GUI can not built.
+ *
+ *        Created:  2020-05-05
+
+ *         Author:  Dilawar Singh <dilawar.s.rajput@gmail.com>
+ */
+
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+    std::cout << "This package does not have a GUI. ";
+    std::cout << "Only Python module is available." << std::endl;
+    return 0;
+}
