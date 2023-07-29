a260c0bb2daa31776ad7e875d26f507c1bfc591d
Alois SCHLOEGL <alois.schloegl@ist.ac.at>
2023-07-29 12:08:33 +0200

    Bump version to 0.16.2; update autoconf generated files

--- stfconf.h
+++ stfconf.h
@@ -7,11 +7,11 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
@@ -44,7 +44,7 @@
 #define PACKAGE_NAME "stimfit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "stimfit 0.16.2"
+#define PACKAGE_STRING "stimfit 0.16.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "stimfit"
@@ -53,10 +53,12 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.16.2"
+#define PACKAGE_VERSION "0.16.3"
 
-/* Define to 1 if you have the ANSI C header files. */
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.16.2"
+#define VERSION "0.16.3"
