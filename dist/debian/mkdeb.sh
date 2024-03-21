a10bf70f3676a5aafbcb774408918de120e192d7
Alois Schloegl <alois.schloegl@gmail.com>
2024-03-21 21:56:10 +0100

    Bump version to 0.16.4

--- dist/debian/mkdeb.sh
+++ dist/debian/mkdeb.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=0.16.3
+VERSION=0.16.4
 
 make dist
 mkdir -p ../deb/
--- stfconf.h
+++ stfconf.h
@@ -44,7 +44,7 @@
 #define PACKAGE_NAME "stimfit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "stimfit 0.16.3"
+#define PACKAGE_STRING "stimfit 0.16.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "stimfit"
@@ -53,7 +53,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.16.3"
+#define PACKAGE_VERSION "0.16.4"
 
 /* Define to 1 if all of the C90 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
@@ -61,4 +61,4 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.16.3"
+#define VERSION "0.16.4"
