3bb3de930ef48bb73c3cd3fd4c8706e20d3c03f0
Alois Schlögl <alois.schloegl@gmail.com>
2025-02-17 07:55:51 +0100

    mkdeb and mkquick are generated by autoconf; remove autogenerated files from repo

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-#! /bin/bash
 GSED=`which gsed`
 if [ "${GSED}" = "" ]
 then
@@ -8,5 +7,6 @@ then
 fi
 
 CMD="find . -type f  \( -name stfconf.h -o -name index.xml -o -name configure.ac -o -name conf.py -o -name installer.nsi -o -name Home.html -o -name stimfit.plist.in -o -name mkdeb.sh -o -name update-deb.sh -o -name mkquick.sh -o -name mkimage.sh -o -name stimfit.1 -o -name Doxyfile -o -name insert_checksums.sh -o -name setup.py -o -path */py-stfio*/meta.yaml \) -exec $GSED -i 's/$1/$2/' {} +"
-echo ${CMD}
-eval ${CMD}
+# echo ${CMD}
+echo "Change 1st line in configure.ac and re-run ./autogen.sh - autoconf is supposed to update the version number in all occurrences."
+# eval ${CMD}
