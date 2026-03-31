4329757a6f9305afcf5271aab7a0810bc6725b8b
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2026-03-31 08:50:40 +0200

    Modernize stfio Python API and tests

--- src/pystfio/unittest_stfio.py
+++ src/pystfio/unittest_stfio.py
@@ -1,119 +1,84 @@
+"""Tests for the standalone `stfio` Python module.
+
+The fixture file [`test.h5`](src/pystfio/test.h5) contains a recording with:
+- 4 channels
+- 3 sections per channel
+- 40000 data points per section
+- sampling interval 0.05 ms
 """
-unittest_stfio.py
 
-Tue, 20 Jul 2010 09:38:19 +0200
-
-The unittest for stfio module created by Christoph,
-the stfio module is a Stimfit-independent python module to read/write
-electrophysiological data from/to different file formats.
-
-Note that to execute this test you need the following files:
-test.h5
-test.abf
-test.dat
-
-These files correspond to the same recording with different file 
-extensions; the recording contains the following properties:
- 
-Number of channels = 4
-Number of traces = 3
-Sampling interval  = 0.05
- 
-"""
-import numpy as np
+from pathlib import Path
 import unittest
+
+import numpy as np
 
 import stfio
 
-rec = stfio.read('test.h5')
+TEST_DATA = Path(__file__).with_name("test.h5")
+MISSING_DATA = Path(__file__).with_name("test.txt")
+
+
 class DataListTest(unittest.TestCase):
-    
-    def testReadH5(self):
-        """ testReadH5() Read HDF5 file system """
-        try:
-            rec = stfio.read('test.h5')
-        except SyntaxError:
-            rec = None
 
-        # test if Recording object was created
-        self.assertTrue(True, isinstance(rec, stfio.Recording))
+    @classmethod
+    def setUpClass(cls):
+        cls.rec = stfio.read(TEST_DATA)
 
-    # def testReadCFS(self):
-    #     """ testReadCFS() Read CED filling system files"""
-    #     try:
-    #         rec = stfio.read('test.dat')
-    #     except SyntaxError:
-    #         rec = None
+    def test_read_h5(self):
+        """HDF5 recordings can be read into a Recording object."""
+        rec = stfio.read(TEST_DATA)
+        self.assertIsInstance(rec, stfio.Recording)
 
-    #     # test if Recording object was created
-    #     self.assertTrue(True, isinstance(rec, stfio.Recording))
+    def test_read_accepts_pathlike(self):
+        """`read()` accepts pathlib.Path inputs via os.PathLike support."""
+        rec = stfio.read(TEST_DATA)
+        self.assertIsInstance(rec, stfio.Recording)
 
-    # def testReadATF(self):
-    #     """ testReadATF() Read ABF files"""
-    #     try:
-    #         rec = stfio.read('test.atf')
-    #     except SyntaxError:
-    #         rec = None
+    def test_open_supports_context_manager(self):
+        """`open()` supports modern `with ... as ...` usage."""
+        with stfio.open(TEST_DATA) as rec:
+            self.assertIsInstance(rec, stfio.Recording)
+            self.assertEqual(4, len(rec))
 
-    #     # test if Recording object was created
-    #     self.assertTrue(True, isinstance(rec, stfio.Recording))
+    def test_read_unsupported_file_raises(self):
+        """Reading an unsupported or missing file raises StfIOException."""
+        with self.assertRaises(stfio.StfIOException):
+            stfio.read(MISSING_DATA)
 
-    def testReadStfException(self):
-        """ Raises a StfException if file format to read is not supported"""
+    def test_write_unsupported_format_returns_false(self):
+        """Writing to an unsupported target format reports failure."""
+        res = self.rec.write('new.abf', 'abf')
+        self.assertEqual(False, res)
 
-        # should raise an StfIOException if filetype is not supported
-        self.assertRaises(stfio.StfIOException, stfio.read, 'test.txt')
+    def test_number_of_channels(self):
+        self.assertEqual(4, len(self.rec))
 
+    def test_number_of_sections(self):
+        self.assertEqual(3, len(self.rec[0]))
 
-    def testWrite(self):
-        """ testWrite() Returns False if file format
-            to write is not supported"""
+    def test_number_of_data_points(self):
+        self.assertEqual(40000, len(self.rec[0][0]))
 
-        # should raise an StfException if filetype is not supported
-        rec = stfio.read('test.h5')
-        res = rec.write('new.abf', 'abf')
-        self.assertEquals(False, res)
+    def test_units(self):
+        self.assertEqual('mV', self.rec[0].yunits)
+        self.assertEqual('pA', self.rec[1].yunits)
+        self.assertEqual('ms', self.rec.xunits)
 
-    def testNumberofChannels(self):
-        """ testNumberofChannels() returns the number of channels """
-        self.assertEquals(4,len(rec))
+    def test_array_creation(self):
+        arr = np.asarray(self.rec[0][0].asarray())
+        self.assertIsInstance(arr, np.ndarray)
+        self.assertEqual((40000,), arr.shape)
 
-    def testNumberofSections(self):
-        """ testNumberofSections() returns the number of channels """
-        self.assertEquals(3,len(rec[0]))
+    def test_channel_name(self):
+        names = [self.rec[i].name for i in range(len(self.rec))]
+        self.assertEqual(names, ['Amp1', 'Amp2', 'Amp3', 'Amp4'])
 
-    def testNumberofDataPoints(self):
-        """ testNumberofDataPoints() returns the number of sampling points """
-        self.assertEquals(40000,len(rec[0][0]))
+    def test_sampling_rate(self):
+        self.assertAlmostEqual(0.05, self.rec.dt, places=3)
 
-    def testunits(self):
-        """ testunits() returns the units in the X/Y axis """
-        self.assertEquals('mV',rec[0].yunits)
-        self.assertEquals('pA',rec[1].yunits)
-        self.assertEquals('ms',rec.xunits)
+    def test_time(self):
+        self.assertEqual(self.rec.time, '23:24:42')
 
-    def testArrayCreation(self):
-        """ testArrayCreation() creation of a numpy array""" 
-        self.assertTrue(type(rec[0][0].asarray()), type(np.empty(0)))
-
-    def testChannelName(self):
-        """ testChannelName() returns the names of the channels """
-        names = [rec[i].name for i in range(len(rec))]
-        mynames = ['Amp1', 'Amp2', 'Amp3', 'Amp4']
-        self.assertEquals(names, mynames)
-
-    def testSamplingRate(self):
-        """ testSamplingRate() returns the sampling rate """
-        self.assertAlmostEqual(0.05, rec.dt, 3)
-
-    def testDate(self):
-        """ testDate() returns the creation date """
-        self.assertEquals(rec.date, '19/07/10')
-
-    def testTime(self):
-        """ testTime() returns the creation time """
-        self.assertEquals(rec.time, '23:24:42')
 
 if __name__ == '__main__':
-    # test all cases
     unittest.main()
